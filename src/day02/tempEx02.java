1ae7c6235ff64df3a3816f2e1b4e218f6ad5f85a
geeunii <koo4934@gmail.com>
2025-07-24 13:42:41 +0900

    day02

---
+++ src/day02/tempEx02.java
@@ -0,0 +1,21 @@
+package day02;
+
+public class tempEx02 {
+
+    public static void main(String[] args) {
+
+        int x, y, temp;
+
+        x = 3;
+        y = 5;
+        temp = 0;
+
+        System.out.printf("%d, %d, %d \n", x, y, temp);
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.printf("%d, %d, %d \n", x, y, temp);
+    }
+}
