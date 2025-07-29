8868ee868fac5639801678ca3c5fe1540146ad9e
geeunii <koo4934@gmail.com>
2025-07-29 12:18:55 +0900

    2025.07.29

---
+++ src/day05/whileEx/DoWhileEx01.java
@@ -0,0 +1,21 @@
+package day05.whileEx;
+
+import java.util.Scanner;
+
+public class DoWhileEx01 {
+    public static void main(String[] args) {
+
+        int x = 20;
+        int sum = 0;
+
+        do {
+            System.out.println("현재 x : " + x);
+            sum += x;
+            x--;
+            System.out.println("처리 후 x, sum : " + x + ", " + sum);
+        } while (10 <= x);
+        System.out.println("---------------------");
+        System.out.println("최종 sum : " + sum);
+
+    }
+}
