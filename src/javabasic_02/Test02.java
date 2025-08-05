8e47cd72762de9bd12bd29eb37ab10715cb0bd1b
geeunii <koo4934@gmail.com>
2025-08-05 11:04:58 +0900

    2025. 08. 05 Test 및 문제풀이

---
+++ src/javabasic_02/Test02.java
@@ -0,0 +1,24 @@
+package javabasic_02;
+
+public class Test02 {
+    public static void main(String[] args) {
+        int[][] array = {
+                {95, 86},
+                {83, 92, 96},
+                {78, 83, 93, 87, 88}
+        };
+
+        int sum = 0, cnt = 0;
+        double avg = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                sum += array[i][j];
+                cnt++;
+            }
+        }
+        avg = (double) sum / cnt;
+        System.out.println("합 : " + sum);
+        System.out.printf("평균 : %.1f", avg);
+    }
+}
---
+++ src/javabasic_02/day10_0805/Ex08.java
@@ -0,0 +1,11 @@
+package javabasic_02.day10_0805;
+
+public class Ex08 {
+    public static void main(String[] args) {
+        int[][] array = {{}, {}, {}};
+
+
+
+
+    }
+}
