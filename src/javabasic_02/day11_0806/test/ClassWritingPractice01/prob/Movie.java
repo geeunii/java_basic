02ddbacf970998a4f9e1d7b63c95a606438770f9
geeunii <koo4934@gmail.com>
2025-08-06 13:00:50 +0900

    2025. 08. 06 Test

---
+++ src/javabasic_02/day11_0806/test/ClassWritingPractice01/prob/Movie.java
@@ -0,0 +1,15 @@
+package javabasic_02.day11_0806.test.ClassWritingPractice01.prob;
+
+public class Movie {
+    void setTitle(String title) {
+        System.out.print(title);
+    }
+
+    void setGenre(String genre) {
+        System.out.print(genre);
+    }
+
+    void play() {
+        System.out.println(" 상영중입니다.");
+    }
+}
