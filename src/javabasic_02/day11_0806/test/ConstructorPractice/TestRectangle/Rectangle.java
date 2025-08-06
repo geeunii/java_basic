b137a368e7557936671b679a59980fadb8e37eb7
geeunii <koo4934@gmail.com>
2025-08-06 16:42:12 +0900

    2025. 08. 06 Test

---
+++ src/javabasic_02/day11_0806/test/ConstructorPractice/TestRectangle/Rectangle.java
@@ -0,0 +1,39 @@
+package javabasic_02.day11_0806.test.ConstructorPractice.TestRectangle;
+
+public class Rectangle {
+    private int width; // 가로 길이
+    private int length; // 세로 길이
+    private String color;
+
+    public int area() {     // 넓이
+        return width * length;
+    }
+
+    public int perimeter() {
+        return (width + length) * 2;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
