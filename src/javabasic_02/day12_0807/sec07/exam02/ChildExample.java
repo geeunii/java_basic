aa346f55230338ee0d30c7cefe83bd1922af829d
geeunii <koo4934@gmail.com>
2025-08-07 14:34:11 +0900

    2025. 08. 07

--- src/javabasic_02/day12_0807/sec07/exam02/ChildExample.java
+++ src/javabasic_02/day12_0807/sec07/exam02/ChildExample.java
@@ -1,14 +1,22 @@
 package javabasic_02.day12_0807.sec07.exam02;
 
 public class ChildExample {
-	public static void main(String[] args) {
-		//자식 객체 생성
+    public static void main(String[] args) {
+        //자식 객체 생성
+        Child child = new Child();
+        child.method1();
+
+        //자동 타입 변환
+        Parent parent = child;
+
+        //메소드 호출
+        parent.method1();
+        parent.method2();
+
+        Child child1 = (Child) parent;
+        child1.method3();
+        System.out.println(child.childName);
 
 
-		//자동 타입 변환
-
-
-		//메소드 호출
-
-	}
+    }
 }
--- src/javabasic_02/day12_0807/sec07/exam03/ChildExample.java
+++ src/javabasic_02/day12_0807/sec07/exam03/ChildExample.java
@@ -2,16 +2,21 @@ package javabasic_02.day12_0807.sec07.exam03;
 
 public class ChildExample {
 	public static void main(String[] args) {
-		//객체 생성 및 자동 타입 변환
+		// 부모 타입으로 자식객체를 생성하세요
+		Parent parent = new Child();
 
-		
 		//Parent 타입으로 필드와 메소드 사용
-
+		parent.field1 = "Data1111";
+		parent.method1();
+		parent.method2();
+//		child.field2 = "Data2222";
+//		child.method3();
 		
 		//강제 타입 변환
-
+		Child child = (Child) parent;
 		
 		//Child 타입으로 필드와 메소드 사용
-
+		child.field2 = "Data2222";
+		child.method3();
 	}
 }
