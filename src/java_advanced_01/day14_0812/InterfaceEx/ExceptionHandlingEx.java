706ca16e09321d1831c2ac4221467bf257db94c0
geeunii <koo4934@gmail.com>
2025-08-12 15:00:12 +0900

    2025. 08. 12 예외 처리

---
+++ src/java_advanced_01/day14_0812/InterfaceEx/ExceptionHandlingEx.java
@@ -0,0 +1,23 @@
+package java_advanced_01.day14_0812.InterfaceEx;
+
+public class ExceptionHandlingEx {
+
+    public static void main(String[] args) {
+        System.out.println("[프로그램 시작]");
+        try {
+            printLength("This is Exception Class");
+            printLength(null);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.fillInStackTrace(); // e.printStackTrace();
+        } finally {
+            System.out.println("마무리 블럭");
+        }
+        System.out.println("프로그램 종료");
+    }
+
+    static void printLength(String param) {
+        int strLength = param.length();
+        System.out.println("전달받은 문자열의 길이 " + strLength);
+    }
+}
