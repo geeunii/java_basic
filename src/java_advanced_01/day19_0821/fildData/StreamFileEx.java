302d89148dd64bc825fb797f3c7738c94f7ad4fc
Geuni <koo4934@gmail.com>
2025-08-21 15:47:42 +0900

    2025. 08. 21 Stream

--- src/java_advanced_01/day19_0821/fildData/StreamFileEx.java
+++ src/java_advanced_01/day19_0821/fildData/StreamFileEx.java
@@ -1,7 +1,5 @@
 package java_advanced_01.day19_0821.fildData;
 
-import lombok.SneakyThrows;
-
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,6 +12,7 @@ public class StreamFileEx {
         Path path = Paths.get(StreamFileEx.class.getResource("data.txt").toURI());
         Stream<String> stream = Files.lines(path, Charset.defaultCharset());
         stream.forEach(line -> System.out.println(line));
+        // stream.forEach(System.out::println);
         stream.close();
     }
 }
