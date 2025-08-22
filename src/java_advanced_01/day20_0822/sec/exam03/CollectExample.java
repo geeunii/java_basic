e4f68d06c2e99f6c0e098cd95d9339ade79318fc
Geuni <koo4934@gmail.com>
2025-08-22 16:24:44 +0900

    2025. 08. 22 OptinalExam

---
+++ src/java_advanced_01/day20_0822/sec/exam03/CollectExample.java
@@ -0,0 +1,16 @@
+package java_advanced_01.day20_0822.sec.exam03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectExample {
+	public static void main(String[] args) {
+		List<Student> totalList = new ArrayList<>();
+		totalList.add(new Student("홍길동", "남", 92));
+		totalList.add(new Student("김수영", "여", 87));
+		totalList.add(new Student("감자바", "남", 95));
+		totalList.add(new Student("오해영", "여", 93));
+		
+
+	}
+}
